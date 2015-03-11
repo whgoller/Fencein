88995bb64bb12cb41e54dbeb80da3ca1511f70ea
wgo <whgoller@gmail.com>
2015-03-10 23:05:00 -0600

    first commit

---
+++ js/mainController.js
@@ -0,0 +1,9 @@
+var app = angular.module('fenceIn');
+
+app.controller("MainController", function($scope){
+    $scope.tournamentName;
+    $scope.tournamentlogin = function(){
+      href="welcome.html";
+    };
+
+});
