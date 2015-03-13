7e12e37c794646af9656597d1abacd526ef3b82d
wgo <whgoller@gmail.com>
2015-03-12 20:20:56 -0600

    working no route

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
-var app = angular.module('fencin', ['ngRoute']);
+var app = angular.module('fencin', []);
         
-app.config(function($routeProvider, $httpProvider){
-  //$httpProvider.interceptors.push('httpRequestInterceptor');
-  
-  $routeProvider
-    .when('/', {
-      templateUrl: 'index.html',
-      controller: 'mainController'     
-  }).when('/tournamentSelection', {
-      templateUrl: 'pages/tournaments/tournaments.html',
-      controller: 'tournamentsController',
-      resolve: {
-        
-        }
-  }).otherwise({
-    redirectTo: '/'
-  });
-
-});
+//app.config(function($routeProvider, $httpProvider){
+//  $httpProvider.interceptors.push('httpRequestInterceptor');
+//  
+//  $routeProvider
+//    .when('/tournamentSelection', {
+//      templateUrl: 'pages/tournaments/tournaments.html',
+//      controller: 'tournamentsController',
+//      resolve: {
+//        
+//        }
+//  }).otherwise({
+//    redirectTo: '/'
+//  });
+//
+//});
