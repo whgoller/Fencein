3215207827988ecc4f1c2bd7e89073ea7fbc47b1
wgo <whgoller@gmail.com>
2015-03-12 22:23:36 -0600

    Removing pages folder from outer layer because it was put into the js folder.

--- pages/backroom/backroomController.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('fencin');
-        
-app.controller('backroomController', function($scope){
-  
-});
--- pages/tournaments/tournamentsController.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('fencin');
-        
-app.controller('tournamentsController', function($scope){
-  
-});
