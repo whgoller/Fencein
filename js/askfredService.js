5a12d93ff6eb0d69646966745d55b721ec914f18
wgo <whgoller@gmail.com>
2015-03-14 12:53:46 -0600

    getting list of fencers per tournament event sucessfull

--- js/askfredService.js
+++ js/askfredService.js
@@ -7,52 +7,57 @@ app.service('askfredService', function ($http, $q, keys) {
     this.format = '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100';
 
     this.getTournaments = function (tournamentName) {
-        var deffered = $q.defer();
-        $http({
-            method: 'jsonp',
-            url: 'https://api.askfred.net/v1/tournament?_api_key=' + this.apiKey + '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100&location=' + tournamentName
-        }).then(function (data) {
-            console.log('askfredservice getTournaments', data.data.tournaments);
-            deffered.resolve(data.data.tournaments);
-        });
-        return deffered.promise;
+      return $http({
+          method: 'jsonp',
+          url: 'https://api.askfred.net/v1/tournament?_api_key=' + this.apiKey + '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100&location=' + tournamentName
+      }).then(function (response) {
+          console.log('askfredservice getTournaments', response.data.tournaments);
+          return response.data.tournaments;
+      });
     };
 
     this.getSingleTournamentEvents = function (tournamentId) {
-        var deffered = $q.defer();
-        $http({
-            method: 'jsonp',
-            url: 'https://api.askfred.net/v1/tournament/' + tournamentId + '?_api_key=' + this.apiKey + '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100'
-        }).then(function (data) {
-            console.log('askfredservice getSingleTournamentEvents', data.data.tournament.events);
-            deffered.resolve(data.data.tournament.events);
-        });
-        return deffered.promise;
+      return $http({
+          method: 'jsonp',
+          url: 'https://api.askfred.net/v1/tournament/' + tournamentId + '?_api_key=' + this.apiKey + '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100'
+      }).then(function (response) {
+          console.log('askfredservice getSingleTournamentEvents', response.data.tournament.events);
+          return response.data.tournament.events;
+      });
     };
+  
+    this.getPreRegisteredFencersInEvent = function(eventId){
+      return $http({
+          method: 'jsonp',
+          url: 'https://api.askfred.net/v1/event/' + eventId + '?_api_key=' + this.apiKey + '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100'
+      }).then(function (response) {
+          console.log('askfredservice getPreRegisteredFencersInEvent', response.data.event.preregs);
+          return response.data.event.preregs
+      });
+    };
+  
 
     this.getAthleteByID = function (usfaID) {
-        var deffered = $q.defer();
-        $http({
-            method: 'jsonp',
-            url: this.fencer + this.apiKey + '&usfa_id=' + usfaID + this.format
-        }).then(function (data) {
-            console.log('askfredservice getAthleteByID', data.data.fencers[0]);
-            deffered.resolve(data.data.fencers[0]);
-        });
-        return deffered.promise;
+      return $http({
+          method: 'jsonp',
+          url: this.fencer + this.apiKey + '&usfa_id=' + usfaID + this.format
+      }).then(function (response) {
+          console.log('askfredservice getAthleteByID', response.data.fencers[0]);
+          return response.data.fencers[0];
+      });
     };
     
-        this.getAthleteByName = function (firstName, lastName) {
-        var deffered = $q.defer();
-        $http({
-            method: 'jsonp',
-            url: this.fencer + this.apiKey + '&first_name_contains=' + firstName + '&last_name_contains=' + lastName + this.format
-        }).then(function (data) {
-            console.log('askfredservice url', url);
-            deffered.resolve(data.data.fencers[0]);
-        });
-        return deffered.promise;
+    this.getAthleteByName = function (firstName, lastName) {
+      return $http({
+          method: 'jsonp',
+          url: this.fencer + this.apiKey + '&first_name_contains=' + firstName + '&last_name_contains=' + lastName + this.format
+      }).then(function (response) {
+          console.log('askfredservice url', url);
+          response.data.fencers[0];
+      });
     };
+  
+  
 
 
 });
--- js/pages/tournaments/tournamentsSelectionController.js
+++ js/pages/tournaments/tournamentsSelectionController.js
@@ -3,29 +3,40 @@ var app = angular.module('fencin');
 app.controller('tournamentSelectionController', function($scope, askfredService){
   $scope.tournamentName = 'Utah Swords Academy Fencing Club';
   $scope.tournaments = [];
-  
-  $scope.selectAction = function() {
-    console.log($scope.selectedTournament);
-    $scope.getTournamentData($scope.selectedTournament);
-  };
+  $scope.events = [];
   
   $scope.getTournamentsList = function(){
     askfredService.getTournaments($scope.tournamentName).then(function(response){
-      //console.log('ctrl', response);
       $scope.tournaments = response;
       console.log('$scope.tournaments', $scope.tournaments);
     });
   }();
   
-  $scope.getTournamentData = function(selectedTournamentId){
-    askfredService.getSingleTournamentEvents(selectedTournamentId).then(function(response){
-      $scope.selectedTournamentInfo = response;
-      
-      console.log('$scope.selectedTournamentInfo', $scope.selectedTournamentInfo);
+  
+  $scope.getTournamentEvents = function(selectedTournamentId){
+    askfredService.getSingleTournamentEvents(selectedTournamentId).then(function(events){
+      $scope.events = events; 
+      events.map(function(event){
+        askfredService.getPreRegisteredFencersInEvent(event.id).then(function(preRegFencers){
+          event.preRegisteredFencers = preRegFencers
+        });
+      });
+      console.log('$scope.events', $scope.events);
     });
   }
   
+  $scope.getEventFencers = function(eventId){
+    askfredService.getPreRegisteredFencersInEvent(eventId).then(function(response){
+      $scope.fencers = response;
+      console.log('$scope.fencers', $scope.fencers);
+    });
+  }
   
+  $scope.selectAction = function() {
+    console.log($scope.selectedTournament);
+    $scope.getTournamentEvents($scope.selectedTournament);
+    
+  }
   
   
   
