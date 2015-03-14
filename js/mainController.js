54b4015ac9eabf1c7b1c73bc77cf1ff74f84c236
ericallen55 <eric.holly.allen@gmail.com>
2015-03-14 12:16:51 -0600

    Checkin Model

--- js/mainController.js
+++ js/mainController.js
@@ -2,9 +2,20 @@ var app = angular.module('fencin');
 
 app.controller("mainController", function($scope, askfredService){
     $scope.tournamentName = 'Utah Swords Academy Fencing Club';
+    $scope.tournaments = ['one', 'two'];
+    console.log('tournaments', $scope.tournaments);
   
     $scope.tournamentlogin = function(){
       href="welcome.html";
+    };
+    
+    $scope.checkIn = function (){
+      console.log('checkIn');  
+    };
+    
+    $scope.tournamentSelected = function (){
+      window.location.hash = '/checkin';
+      $('#checkinModal').modal('hide');     //hides the model
     };
   
   
--- js/pages/checkin/checkinController.js
+++ js/pages/checkin/checkinController.js
@@ -1,22 +1,30 @@
 var app = angular.module('fencin');
 
-app.controller('checkInController', function ($scope, askfredService) {
-    $scope.tournaments = ['one', 'two', 'three'];
-    console.log('checkInController');
-    $scope.getTournamentData = function () {
-        console.log('getTournamentData');
-        $scope.events = askfredService.getTournamentData($scope.selectedTournament);
-    };
-    $scope.getAthlete = function () {
-        if ($scope.athlete.usfaID) {
-            $scope.athlete = askfredService.getAthleteByID($scope.athlete.usfaID);
-        }
-        else if ($scope.athlete.firstName && $scope.athlete.lastName) {
-            askfredService.getAthleteByName($scope.athlete.firstName, $scope.athlete.lastName).then(function (response) {
-                $scope.athlete = response;
-            });
+app.controller('checkInController', function ($scope, firebaseService) {
+   
 
-            console.log('getAthlete', $scope.athlete);
-        }       
-    };
+    $scope.getParticipants = function (){
+         $scope.participants = [{firstName: 'bob', lastName: 'sam'},{firstName: 'fred', lastName: 'both'}]; //TODO call firebase
+    }();    //self calling function
+    
+    
+    
+//    $scope.tournaments = ['one', 'two', 'three'];
+//    console.log('checkInController');
+//    $scope.getTournamentData = function () {
+//        console.log('getTournamentData');
+//        $scope.events = askfredService.getTournamentData($scope.selectedTournament);
+//    };
+//    $scope.getAthlete = function () {
+//        if ($scope.athlete.usfaID) {
+//            $scope.athlete = askfredService.getAthleteByID($scope.athlete.usfaID);
+//        }
+//        else if ($scope.athlete.firstName && $scope.athlete.lastName) {
+//            askfredService.getAthleteByName($scope.athlete.firstName, $scope.athlete.lastName).then(function (response) {
+//                $scope.athlete = response;
+//            });
+//
+//            console.log('getAthlete', $scope.athlete);
+//        }       
+//    };
 });
