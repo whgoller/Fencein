6b54fa4175c0fa48181fbc44f4a7b9834883623f
ericallen55 <eric.holly.allen@gmail.com>
2015-03-14 12:35:11 -0600

    Checkin participant table

--- js/pages/checkin/checkinController.js
+++ js/pages/checkin/checkinController.js
@@ -1,14 +1,19 @@
 var app = angular.module('fencin');
 
-app.controller('checkInController', function ($scope, firebaseService) {
-   
+app.controller('checkInController', function ($scope) {
 
-    $scope.getParticipants = function (){
-         $scope.participants = [{firstName: 'bob', lastName: 'sam'},{firstName: 'fred', lastName: 'both'}]; //TODO call firebase
+
+    $scope.getParticipants = function () {
+        $scope.participants = [{firstName: 'bob', lastName: 'sam'}, {firstName: 'fred', lastName: 'both'}]; //TODO call firebase
     }();    //self calling function
-    
-    
-    
+
+    $scope.getParticipant = function (participant) {
+        $scope.currentParticipant = participant;
+        console.log('$scope.currentParticipant', $scope.currentParticipant)
+        window.location.hash = '/checkinParticipant';
+    };
+
+
 //    $scope.tournaments = ['one', 'two', 'three'];
 //    console.log('checkInController');
 //    $scope.getTournamentData = function () {
