7deb5540e40635c14d1beeb9edffb1aac1e33550
wgo <whgoller@gmail.com>
2015-03-13 18:35:13 -0600

    adding firebaseService

---
+++ js/firebaseService.js
@@ -0,0 +1,23 @@
+var app = angular.module('fencin');
+        
+app.service('firebaseService', function ($firebase) {
+  var firebaseUrl = 'https://fencein.firebaseio.com/';
+  
+  
+  // Not sure if the following are correct yet for our data structure. we can talk about it on Sat.
+
+  this.getClub = function(clubId){
+    return $firebase(new Firebase(firebaseUrl + 'clubs/' + clubId)).$asObject();
+  };
+
+  this.getTournament = function(clubId){
+    return $firebase(new Firebase(firebaseUrl + 'clubss/' + clubId + '/tournaments')).$asArray();
+  }
+  
+  this.getTournamentEvents = function(clubId, tournamentId){
+    return $firebase(new Firebase(firebaseUrl + 'clubss/' + clubId + '/tournaments/' + tournamentId)).$asArray();
+  }
+  
+  
+  
+})
