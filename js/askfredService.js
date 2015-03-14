32fcf87b22e6a6bde20dd1d094c48fcbb6413280
ericallen55 <eric.holly.allen@gmail.com>
2015-03-14 10:50:10 -0600

    adding calls to askfred

--- js/askfredService.js
+++ js/askfredService.js
@@ -1,31 +1,58 @@
 var app = angular.module('fencin');
-        
-app.service('askfredService', function($http, $q, keys){
-  this.apiKey = keys.getFredApi;
-  
-  this.getTournaments = function(tournamentName){
-    var deffered = $q.defer();
-    $http({
-      method: 'jsonp',
-      url: 'https://api.askfred.net/v1/tournament?_api_key=' + this.apiKey + '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100&location=' + tournamentName      
-    }).then(function(data){
-      console.log('askfredservice getTournaments', data.data.tournaments)
-      deffered.resolve(data.data.tournaments);
-    });
-    return deffered.promise;
-  }
-  
-  this.getSingleTournamentEvents = function(tournamentId){
-    var deffered = $q.defer();
-    $http({
-      method: 'jsonp',
-      url: 'https://api.askfred.net/v1/tournament/' + tournamentId + '?_api_key=' + this.apiKey + '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100'     
-    }).then(function(data){
-      console.log('askfredservice getSingleTournamentEvents', data.data.tournament.events)
-      deffered.resolve(data.data.tournament.events);
-    });
-    return deffered.promise;
-  }
-  
-  
+
+app.service('askfredService', function ($http, $q, Keys) {
+    this.apiKey = Keys.getFredApi;
+    this.tournament = 'https://api.askfred.net/v1/tournament';
+    this.fencer = 'https://api.askfred.net/v1/fencer?_api_key=';
+    this.format = '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100';
+
+    this.getTournaments = function (tournamentName) {
+        var deffered = $q.defer();
+        $http({
+            method: 'jsonp',
+            url: 'https://api.askfred.net/v1/tournament?_api_key=' + this.apiKey + '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100&location=' + tournamentName
+        }).then(function (data) {
+            console.log('askfredservice getTournaments', data.data.tournaments);
+            deffered.resolve(data.data.tournaments);
+        });
+        return deffered.promise;
+    };
+
+    this.getSingleTournamentEvents = function (tournamentId) {
+        var deffered = $q.defer();
+        $http({
+            method: 'jsonp',
+            url: 'https://api.askfred.net/v1/tournament/' + tournamentId + '?_api_key=' + this.apiKey + '&_format=jsonp&_jsonp_callback=JSON_CALLBACK&_per_page=100'
+        }).then(function (data) {
+            console.log('askfredservice getSingleTournamentEvents', data.data.tournament.events);
+            deffered.resolve(data.data.tournament.events);
+        });
+        return deffered.promise;
+    };
+
+    this.getAthleteByID = function (usfaID) {
+        var deffered = $q.defer();
+        $http({
+            method: 'jsonp',
+            url: this.fencer + this.apiKey + '&usfa_id=' + usfaID + this.format
+        }).then(function (data) {
+            console.log('askfredservice getAthleteByID', data.data.fencers[0]);
+            deffered.resolve(data.data.fencers[0]);
+        });
+        return deffered.promise;
+    };
+    
+        this.getAthleteByName = function (firstName, lastName) {
+        var deffered = $q.defer();
+        $http({
+            method: 'jsonp',
+            url: this.fencer + this.apiKey + '&first_name_contains=' + firstName + '&last_name_contains=' + lastName + this.format
+        }).then(function (data) {
+            console.log('askfredservice url', url);
+            deffered.resolve(data.data.fencers[0]);
+        });
+        return deffered.promise;
+    };
+
+
 });
--- js/pages/checkin/checkinController.js
+++ js/pages/checkin/checkinController.js
@@ -1,21 +1,22 @@
 var app = angular.module('fencin');
 
-app.controller('checkInController', function($scope, checkInService) {
+app.controller('checkInController', function ($scope, askfredService) {
     $scope.tournaments = ['one', 'two', 'three'];
     console.log('checkInController');
-
-    $scope.getTournamentData = function() {
+    $scope.getTournamentData = function () {
         console.log('getTournamentData');
-        $scope.events = checkInService.getTournamentData($scope.selectedTournament);
-
+        $scope.events = askfredService.getTournamentData($scope.selectedTournament);
     };
+    $scope.getAthlete = function () {
+        if ($scope.athlete.usfaID) {
+            $scope.athlete = askfredService.getAthleteByID($scope.athlete.usfaID);
+        }
+        else if ($scope.athlete.firstName && $scope.athlete.lastName) {
+            askfredService.getAthleteByName($scope.athlete.firstName, $scope.athlete.lastName).then(function (response) {
+                $scope.athlete = response;
+            });
 
-    $scope.getAthlete = function() {
-        if($scope.athlete.usfaID){
-            $scope.athlete = checkInService.getAthleteByID($scope.athlete.usfaID);
-        }
-        else if($scope.athlete.firstName && $scope.athlete.lastName){
-            $scope.athlete = checkInService.getAthleteByName($scope.athlete.firstName, $scope.athlete.lastName);
-        }
+            console.log('getAthlete', $scope.athlete);
+        }       
     };
 });
