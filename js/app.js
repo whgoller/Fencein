e2f564ec3d234ed83e015cc40d7486ec7bcf96a5
ericallen55 <eric.holly.allen@gmail.com>
2015-03-14 09:27:16 -0600

    Checkin

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
 var app = angular.module('fencin', ['ngRoute']);
-        
-app.config(function($routeProvider, $httpProvider){
-  //$httpProvider.interceptors.push('httpRequestInterceptor');
-  
-  $routeProvider
-    .when('/tournamentSelection', {
-      templateUrl: '/js/pages/tournaments/tournamentSelection.html',
-      controller: 'tournamentSelectionController'//,
+
+app.config(function($routeProvider, $httpProvider) {
+    //$httpProvider.interceptors.push('httpRequestInterceptor');
+
+    $routeProvider
+            .when('/tournamentSelection', {
+        templateUrl: '/js/pages/tournaments/tournamentSelection.html',
+        controller: 'tournamentSelectionController'//,
 //      resolve: {
 //        
 //        }
-  }).when('/backroom', {
-      templateUrl: '/js/pages/backroom/backroom.html',
-      controller: 'backroomController'
-  }).otherwise({
-    redirectTo: '/'
-  });
+    }).when('/checkin', {
+        templateUrl: '/js/pages/checkin/checkin.html',
+        controller: 'checkInController'
+
+    }).when('/backroom', {
+        templateUrl: '/js/pages/backroom/backroom.html',
+        controller: 'backroomController'
+
+    }).otherwise({
+        redirectTo: '/'
+    });
 
 });
