deda8765d36e058aa5c881dc8642ca6e552827c0
ericallen55 <eric.holly.allen@gmail.com>
2015-03-17 17:34:44 -0600

    Backroom format

--- js/pages/backroom/backroomController.js
+++ js/pages/backroom/backroomController.js
@@ -1,5 +1,16 @@
 var app = angular.module('fencin');
-        
-app.controller('backroomController', function($scope){
-  
+
+app.controller('backroomController', function($scope, backroomService) {
+    $scope.addParticipants = backroomService.getAthletes();
+    $scope.addedParticipants = [];
+
+    $scope.addParticipant = function(participant) {
+        $scope.addedParticipants.push(participant);
+        $scope.addParticipants.splice($scope.addParticipants.indexOf(participant), 1);
+    };
+
+    $scope.removeParticipant = function(participant) {
+        $scope.addParticipants.push(participant);
+        $scope.addedParticipants.splice($scope.addedParticipants.indexOf(participant), 1);
+    };
 });
---
+++ js/pages/backroom/backroomService.js
@@ -0,0 +1,22 @@
+var app = angular.module('fencin');
+
+app.service('backroomService', function () {
+    this.getAthletes = function(){
+        this.needToAdd = [
+            {firstName: 'bob',
+                lastName: 'allen',
+                event: 'foil'
+            },
+            {firstName: 'alex',
+                lastName: 'allen',
+                event: 'sword'
+            },
+            {firstName: 'sam',
+                lastName: 'goller',
+                event: 'epee'
+            }
+        ];
+        return this.needToAdd;
+    };    
+});
+
